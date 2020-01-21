ea759c0c3e839211d16ee16f0f15e7a07667fc75
emap <emap@192.168.2.68>
2020-01-21 09:23:51 +0000

    We are improving on the "my post" interface or UI thing.

--- Journalio/gen/com/journalio/android/R.java
+++ Journalio/gen/com/journalio/android/R.java
@@ -14,26 +14,27 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int activity_inputer_id=0x7f060004;
         public static final int activity_inputer_parent_id=0x7f060003;
         public static final int app_header_id=0x7f060001;
-        public static final int base_bttn_0=0x7f060007;
-        public static final int base_bttn_1=0x7f060008;
+        public static final int base_bttn_0=0x7f060008;
+        public static final int base_bttn_1=0x7f060009;
         public static final int base_bttns_parent_id=0x7f060002;
-        public static final int bttn_get_acc=0x7f060012;
-        public static final int bttn_sign_up=0x7f06000f;
-        public static final int et_email=0x7f06000c;
-        public static final int et_pswd_0=0x7f06000d;
-        public static final int et_pswd_1=0x7f06000e;
-        public static final int iv_img=0x7f06000b;
-        public static final int iv_img_cancel=0x7f060010;
+        public static final int bttn_get_acc=0x7f060013;
+        public static final int bttn_sign_up=0x7f060010;
+        public static final int et_email=0x7f06000d;
+        public static final int et_pswd_0=0x7f06000e;
+        public static final int et_pswd_1=0x7f06000f;
+        public static final int iv_img=0x7f06000c;
+        public static final int iv_img_cancel=0x7f060011;
         public static final int listView1=0x7f060000;
-        public static final int news_detail=0x7f060013;
-        public static final int news_hd=0x7f060011;
-        public static final int sv_0=0x7f060009;
-        public static final int sv_r_0=0x7f06000a;
-        public static final int tab_0=0x7f060004;
-        public static final int tab_1=0x7f060005;
-        public static final int tab_2=0x7f060006;
+        public static final int news_detail=0x7f060014;
+        public static final int news_hd=0x7f060012;
+        public static final int sv_0=0x7f06000a;
+        public static final int sv_r_0=0x7f06000b;
+        public static final int tab_0=0x7f060005;
+        public static final int tab_1=0x7f060006;
+        public static final int tab_2=0x7f060007;
     }
     public static final class layout {
         public static final int activity_poster=0x7f030000;
